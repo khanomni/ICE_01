c53a4824fd7aec7fce0d607c2cb27be6a882c14f
amna khan <khan453@sheridancollege.ca>
2021-10-09 14:48:34 -0400

    First Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,7 +9,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card nineHearts = new Card("Hrts", 6);
+       ninehearts.setSuit("Spades");
     }
     
 }
