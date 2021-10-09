3eee6c0e69079aff3efee30df03b17ab63c2c17c
amna khan <khan453@sheridancollege.ca>
2021-10-09 15:47:58 -0400

    First Commit

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,16 @@
+
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+/**
+ * @author Amna Khan
+ * A class to create Cards
+ * Student ID 991556455
+ */
+public class CardTrick {
+
+    public static void main(String[] args)
+    {
+
+    }
+    
+}
